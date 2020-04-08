fd2446c94eda59df67528833c8c1417e631fc6e3
rotelstift <rotelstift@gmail.com>
2020-04-08 15:23:05 +0900

    テーブル名が単数形だったので複数形に直した

--- database/migrations/2020_04_08_024614_create_type_table.php
+++ database/migrations/2020_04_08_024614_create_type_table.php
@@ -13,7 +13,7 @@ class CreateTypeTable extends Migration
      */
     public function up()
     {
-        Schema::create('type', function (Blueprint $table) {
+        Schema::create('types', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name', 120);
             $table->timestamps();
