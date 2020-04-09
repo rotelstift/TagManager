c6c4e8125ec05ef36463a1dd0f3a958114250871
rotelstift <rotelstift@gmail.com>
2020-04-09 12:51:06 +0900

    testが通るようにした

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/',  function () {
+    return view('welcome');
+});
 Route::resource('type', 'TypeController');
 Route::resource('template', 'TemplateController');
