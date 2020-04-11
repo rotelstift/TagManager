151e95c090779792d31fe8d8130c0a6963f0e030
rotelstift <rotelstift@gmail.com>
2020-04-11 15:51:26 +0900

    fakerの使い方を間違えていたので修正

--- database/factories/InformationFactory.php
+++ database/factories/InformationFactory.php
@@ -8,7 +8,7 @@ use Faker\Generator as Faker;
 $factory->define(Information::class, function (Faker $faker) {
     return [
         //
-        "name" => $faker->title,
+        "name" => $faker->sequence(10),
         "template_id" => factory(App\Template::class),
     ];
 });
