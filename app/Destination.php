6d7bdaf4293c2bbc9514f1cf044d30bfa3a01da2
rotelstift <rotelstift@gmail.com>
2020-04-11 16:29:42 +0900

    informationは不可算名詞！（二回め）

--- app/Destination.php
+++ app/Destination.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Destination extends Model
 {
     //
-    public function informations()
+    public function information()
     {
         return $this->belongsToMany('App\Information');
     }
